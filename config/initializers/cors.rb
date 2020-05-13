ecda2068e1b7fd931292aaa46bad7866f5044a05
JBrenneman444 <JBrenneman444@gmail.com>
2020-05-13 15:28:50 -0700

    changing origins URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://aoe-tribute-client.herokuapp.com/' # need 's' because heroku link has it
+    origins 'https://aoe-tribute-client.herokuapp.com' # need 's' because heroku link has it
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
