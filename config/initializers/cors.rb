84058b06b2a0cefa49811283fb808dbd0662cbd1
JBrenneman444 <JBrenneman444@gmail.com>
2020-05-13 15:24:37 -0700

    gems installed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://aoe-tribute-client.herokuapp.com/' # need 's' because heroku link has it
-
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
